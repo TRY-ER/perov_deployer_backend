c6ee1d1198e9a354e9b4d3e36f158a7b1c6fd089
TRY-ER <avyarth00@gmail.com>
2022-07-16 23:55:53 +0530

    Configre and tested the backend api connection successfully

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI, Form, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+import joblib 
+import json 
+
+
 
 
 class SampleForm:
@@ -8,13 +14,29 @@ class SampleForm:
         self.num1 = num1
         self.num2 = num2
 
-
-
+col_data = joblib.load("coldata_serial.z")
+test_data = {
+    "this": "this",
+    "that": "that"
+}
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 @app.get("/predict")
-def test():
-    return{"value":"test value"}
+async def test():
+    return jsonable_encoder(col_data)
 
 @app.post("/predict")
 async def predict(form: SampleForm = Depends()):
